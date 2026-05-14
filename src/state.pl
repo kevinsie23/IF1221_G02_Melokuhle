5e79ddbfa8032212de74f3071dc49bcf863404c2
Najippp <hilmikistijantoro@gmail.com>
2026-05-14 19:33:08 +0700

    fix: fix include and added main.pl

--- src/state.pl
+++ src/state.pl
@@ -1,18 +1,3 @@
-:- include('save.pl').
-:- include('load.pl').
-
-/* -----DYNAMIC VARIABLES----- */
-:- dynamic(discardPile/1).
-:- dynamic(urutanPemain/1).
-:- dynamic(reverseGiliran/1).
-:- dynamic(infoPemain/2).
-:- dynamic(idxGiliran/1).
-:- dynamic(playedDraw/0).
-:- dynamic(playedDrawFour/0).
-:- dynamic(uniCalled/1).
-
-
-
 /* Data buat Test Run */
 start:-
     retractall(discardPile(_)),
