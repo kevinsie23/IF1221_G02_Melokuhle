bbf697ff85fcded19ee9da1dba2d88683fa38511
WimarW <wimarwidiarto123@gmail.com>
2026-05-15 08:51:18 +0700

    refactor: tweak dikit bbrp predikat

--- src/util.pl
+++ src/util.pl
@@ -1,15 +1,11 @@
-% List predikat
-% Append
-% ParseCard
-% LengthList
-% PrintList
-% PrintCard
-
+% Menambah elemen di head
 appendHead(X, List, [X|List]).
 
+% Menambah elemen di akhir
 appendTail(X,[],[X]):- !.
 appendTail(X,[H|T], [H|Result]):- appendTail(X, T, Result).
 
+% Menambah elemen pada Index Idx
 appendAtN(X,0,List,[X|List]):- !.
 appendAtN(X,Idx,[H|T], [H|Result]):- Idx>0, !, NextIdx is Idx-1, appendAtN(X, NextIdx, T, Result).
 
@@ -20,11 +16,13 @@ deleteAtN(Idx, [H|T], Element, [H|Rest]):-
     NextId is Idx - 1,
     removeAt(NextId, T, Element, Rest).
 
+
 lengthList([], 0):- !.
 lengthList([_|T], X):- lengthList(T,X1), X is X1+1.
 
+
+%Menampilkan List
 printList([]):- nl, !.
 printList([H|T]):- write(H), nl, printList(T).
 
-parseCard(kartu(Warna, Jenis), Warna, Jenis).
 printCard(Warna, Jenis):- format('~w-~w~n', [Warna,Jenis]).
